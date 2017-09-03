b2b4b721bb27c10091688c6faca56e31dc4567ed
Remilia Scarlet <remilia.1505@gmail.com>
2017-09-03 20:14:07 +0700

    core33g: Add patches
    * Frameworks patches thanks to @ngoquang2708 for his work on vivalto3gvn
    * Patch applying script from I8552-CM repo:
    https://github.com/I8552-CM/android_device_msm7627a-common/blob/los-14.1/patches/apply.sh
    
    Change-Id: I36691961ab877919913efeccb437de4b335ac9fd
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,6 +18,9 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
+# Patch the source first
+sh device/samsung/core33g/patches/apply.sh
+
 for i in eng user userdebug; do
 add_lunch_combo lineage_core33g-${i};
 done
